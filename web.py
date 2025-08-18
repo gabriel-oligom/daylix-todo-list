5b5c50411a6a930340cf3cfb756d7bf233bda304
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-08-18 20:47:11 -0300

    Implement addition feature to Daylix via web app

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todos():
+    user_new_todo = st.session_state["new_todo"]
+    todos.append(user_new_todo)
+    functions.write_todos(todos)
+
+
 st.title("Daylix - Todo App")
 st.subheader("This is my Task Manager App!!")
 
@@ -11,4 +17,5 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo...") 
+st.text_input(label="", placeholder="Add a new todo...", 
+              on_change=add_todos, key="new_todo") 
