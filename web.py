c649b3240c7df2852888f403f3ff17ceebc9437c
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-08-18 13:43:07 -0300

    Create a static web interface

---
+++ web.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+st.title("Daylix - Todo App")
+st.subheader("This is my Task Manager App!!")
+
+st.write("This app is to increase your productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Add a new todo...") 
